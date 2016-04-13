2d7607580290f031ebccba5726e7d3c8baa50d8d
User <Suhaib@users-mac-2.local>
2016-04-13 12:05:22 +0530

    Completed first tab related to list view with variable cell height depending on text.

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    var data: NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
+        if let path = NSBundle.mainBundle().pathForResource("data", ofType: "plist") {
+            data = NSArray(contentsOfFile: path)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +27,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (data?.count)!;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = table.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        //cell text properties
+        cell.textLabel?.text = (data?.objectAtIndex(indexPath.row) as! String)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.font = UIFont(name:"HelveticaNeue-Bold", size: 16.0)
+        cell.textLabel?.textColor = UIColor.blueColor()
+        return cell
+    }
+    
+   
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension;
+    }
 }
 
